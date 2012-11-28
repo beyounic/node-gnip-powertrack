7d02ac105a37912563c6134b70b73c84b26d5b79
Matteo Agosti <matteo.agosti@gmail.com>
2012-11-28 17:21:16 +0100

    Less verbose logging

--- client.js
+++ client.js
@@ -2,5 +2,5 @@ var config = require('./config'),
     io = require('socket.io-client').connect('http://localhost:' + config.server.port + '/stream');
 
 io.on('data', function(data) {
-  console.log(data);
+  console.log(data.id + ' - ' + data.postedTime);
 });
