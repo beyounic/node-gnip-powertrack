14b8da4bc8b56163d40b182fa7bf19ba81ecb208
Matteo Agosti <matteo.agosti@gmail.com>
2012-11-29 14:02:07 +0100

    Removed temporary test, will be structured in a mocha test

--- client.js
+++
@@ -1,6 +0,0 @@
-var config = require('./config'),
-    io = require('socket.io-client').connect('http://localhost:' + config.server.port + '/stream');
-
-io.on('data', function(data) {
-  console.log(data.id + ' - ' + data.postedTime);
-});
