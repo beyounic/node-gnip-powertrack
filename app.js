d3ab59b2f62c8fbc563d8c13b16e4fe381e483a4
Matteo Agosti <matteo.agosti@gmail.com>
2012-11-29 14:02:59 +0100

    Exposing RulesAPI via socket

--- app.js
+++ app.js
@@ -17,11 +17,26 @@ io.configure(function() {
   io.set('log level', 1);
 });
 
-powerTrack.on('data', function(data) {
-  var rules = data.gnip.matching_rules,
-      i,
-      len,
-      tag;
+io.of('/rules').on('connection', function(socket) {
+  socket.on('get', function(callback) {
+    rulesAPI.get(function(error, data) {
+      if (typeof callback === 'function')
+        callback(error, data);
+    });
+  }).on('add', function(rules, callback) {
+    rulesAPI.add(rules, function(error) {
+      if (typeof callback === 'function')
+        callback(error);
+    });
+  }).on('remove', function(rules, callback) {
+    rulesAPI.remove(rules, function(error) {
+      if (typeof callback === 'function')
+        callback(error);
+    });
+  });
+});
+
+io.of('/stream').on('connection', function(socket) { });
 
   io.of('/stream').emit('data', data);
   for (i = 0, len = rules.length; i < len; i++) {
