ca4d0d27ae77198880a3b06e6ed639e0ef4c3f97
Matteo Agosti <matteo.agosti@gmail.com>
2012-12-05 15:15:46 +0100

    Evaluating rules volume, supporting rate limit parameter, emitting overflow event in case a set of rules exceeds the volume

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var config = require('./config'),
     io = require('socket.io').listen(config.server.port || 5000),
+    util = require('util'),
     gnip = require('./lib/gnip'),
     powerTrack = new gnip.PowerTrack({
       apiUrl: config.gnip.powerTrackUrl,
@@ -10,7 +11,34 @@ var config = require('./config'),
       apiUrl: config.gnip.rulesAPIUrl,
       username: config.gnip.username,
       password: config.gnip.password
-    });
+    }),
+    counters = {};
+
+if (config.server.rate) {
+  if (typeof config.server.rate.max !== 'number' ||
+      typeof config.server.rate.interval !== 'number')
+    throw new Error('Invalid config.server.rate structure. Should be {max: number, interval, number}');
+
+  setInterval(function() {
+    var rulesToRemove = [],
+        i;
+
+    for (i in counters) {
+      if (counters[i] > config.server.rate.max) {
+        rulesToRemove.push({value: i});
+      }
+    }
+
+    if (rulesToRemove.length > 0) {
+      rulesAPI.remove({rules: rulesToRemove}, function(error) {
+        io.of('/stream').emit('overflow', rulesToRemove);
+      });
+    }
+
+    counters = {};
+
+  }, config.server.rate.interval);
+}
 
 io.configure(function() {
   io.set('transports', ['websocket']);
@@ -39,6 +67,15 @@ io.of('/rules').on('connection', function(socket) {
 io.of('/stream').on('connection', function(socket) { });
 
 powerTrack.on('data', function(data) {
+  var i, len, rule;
+
+  if (data.gnip && util.isArray(data.gnip.matching_rules)) {
+    for (i = 0, len = data.gnip.matching_rules.length; i < len; i++) {
+      rule = data.gnip.matching_rules[i].value;
+      counters[rule] = (counters[rule] || 0) + 1;
+    }
+  }
+
   io.of('/stream').emit('data', data);
 }).on('error', function(error) {
   io.of('/stream').emit('error', error);
